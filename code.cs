cc31058639b14ee7938094710cfedffee22a8522
Nikita <infinityOA@yandex.ru>
2023-02-12 17:32:20 +0300

    My first commit

---
+++ code.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace StringArrayExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = new string[] { "дом", "слон", "лес", "команда", "кит" };
+            int count = 0; // Переменная для хранения количества строк с длиной менее или равной 3 символам
