8bb38bad880a1980ccd83b4c54b82fe8ee8309d4
Nikita <infinityOA@yandex.ru>
2023-02-12 17:33:20 +0300

    My second commit

--- code.cs
+++ code.cs
@@ -8,3 +8,15 @@ namespace StringArrayExample
         {
             string[] inputArray = new string[] { "дом", "слон", "лес", "команда", "кит" };
             int count = 0; // Переменная для хранения количества строк с длиной менее или равной 3 символам
+
+            // Создание выходного массива
+            string[] outputArray = new string[count];
+            int j = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i].Length <= 3)
+                {
+                    outputArray[j] = inputArray[i];
+                    j++;
+                }
+            }
